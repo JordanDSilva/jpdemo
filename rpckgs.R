380fcb1015d998c5d70e1e50e61010436d9f7b27
JordanDSilva <jordan.dsilva@research.uwa.edu.au>
2024-11-21 09:04:11 +0800

    updates to aus cran

--- rpckgs.R
+++ rpckgs.R
@@ -1,8 +1,9 @@
 dir.create("~/jpdemo/pckgs", recursive = TRUE)
 .libPaths( c("~/jpdemo/pckgs", .libPaths()) )
 
-install.packages(c("matrixStats", "dplyr", "foreach", "doParallel", "data.table", "Cairo", "utils", "stringr", "remotes"), repos = "http://cran.us.r-project.org")
+install.packages(c("matrixStats", "dplyr", "foreach", "doParallel", "data.table", "Cairo", "utils", "stringr", "remotes"), repos = "https://cran.csiro.au/")
 
+q()
 remotes::install_github("asgr/magicaxis")
 remotes::install_github("asgr/celestial")
 remotes::install_github("asgr/imager")
